c7282c1e2950aca099fda62c9daab970e726bb98
Michael Coyle <mcoyle74@gmail.com>
2016-08-01 10:53:35 -0500

    Copied files from Relationships exercise gist. Wrote query string for $selectTeams variable to populate select element in player-form.phtml based on league radio buttons.

---
+++ public/player-details.php
@@ -0,0 +1,43 @@
+<?php
+require __DIR__ . '/../src/Input.php';
+
+function pageController()
+{
+    $teams = [];
+    if (Input::has('league')) {
+        // Filter teams based on league, only select the team's identifier
+        // and its name
+        $league = Input::get('league');
+        $selectTeams = "SELECT id, name FROM teams WHERE league = '$league'";
+        // Try your query in Sequel Pro
+        var_dump($selectTeams);
+        // Use the values from your query to populate your form
+        // $teams = ...
+    }
+    // The player's identifier should be in the query string
+    $teamId = Input::get('player_id');
+    $sql = '';
+    var_dump($sql);
+
+    return [
+        'title' => 'Chris Young',
+        'teams' => $teams,
+    ];
+}
+extract(pageController());
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../partials/head.phtml' ?>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="page-header"><h1>Chris Young</h1></div>
+                <?php include '../partials/player-form.phtml' ?>
+            </div>
+        </div>
+        <?php include '../partials/scripts.phtml' ?>
+    </body>
+</html>
